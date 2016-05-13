d02e02908e8ed591bf315bf5ea9390461c6e88ab
ioio1255 <sunguoxi218@gmail.com>
2016-05-13 15:55:00 +0900

    Edit the files

--- JsonConvert/JsonConvert/Program.cs
+++ JsonConvert/JsonConvert/Program.cs
@@ -13,6 +13,9 @@ namespace JsonConvertTool
         {
             string jsonOutput="{\"id\":\"108013515952807\",\"posts\":{\"data\":[{\"id\":\"108013515952807_470186843068804\",\"created_time\":\"2013-05-14T20:43:28+0000\"},{\"message\":\"TEKST\",\"id\":\"108013515952807_470178529736302\",\"created_time\":\"2013-05-14T20:22:07+0000\"}";
             var myelement= JsonConvert.DeserializeXmlNode(jsonOutput, "myelement");
+
+
+
             
         }
     }
