f17768ca09dff5702fd117760be26f1f58cfc289
ioio1255 <sunguoxi218@gmail.com>
2016-05-13 15:44:53 +0900

    Add JsonTest

---
+++ JsonConvert/JsonConvert/Program.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonConvertTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonOutput="{\"id\":\"108013515952807\",\"posts\":{\"data\":[{\"id\":\"108013515952807_470186843068804\",\"created_time\":\"2013-05-14T20:43:28+0000\"},{\"message\":\"TEKST\",\"id\":\"108013515952807_470178529736302\",\"created_time\":\"2013-05-14T20:22:07+0000\"}";
+            var myelement= JsonConvert.DeserializeXmlNode(jsonOutput, "myelement");
+            
+        }
+    }
+}
